69ab198d5d35ce173e83d40183fc4fa45f22d114
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2021-11-15 11:39:27 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,9 @@
 
-arry = a.(
-  a='v
-#print("Hello World")
+print("Hello World")
 
-#List = [1, 4, 5, 3, 6, 8, 7, 9, 10]
-#List.sort()
-#print(List)
+List = [1, 4, 5, 3, 6, 8, 7, 9, 10]
+List.sort()
+print(List)
 
 
-#print("Alphonse")
+print("Alphonse")
