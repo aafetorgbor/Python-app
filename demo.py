f5408df7e40cbdf980a6d74a1ce6f45979c8bc4b
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2021-11-15 11:25:29 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
-print("Hello World")
+array=a.9
+b = 'd,}
+#print("Hello World")
 
-List = [1, 4, 5, 3, 6, 8, 7, 9, 10]
-List.sort()
-print(List)
+#List = [1, 4, 5, 3, 6, 8, 7, 9, 10]
+#List.sort()
+#print(List)
 
 
-print("Alphonse")
+#print("Alphonse")
