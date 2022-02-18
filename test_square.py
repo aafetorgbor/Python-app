3d3ef422c4d0a436cd7da7427785d3869f040f5f
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2022-02-18 16:31:03 -0500

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -2,11 +2,11 @@ import math
 
 def test_sqrt():
    num = 25
-   assert math.sqrt(num) == 5
+   assert math.sqrt(num) == 625
 
 def testsquare():
    num = 7
-   assert 7*7 == 49
+   assert 7 * 7 == 49
 
 def tesequality():
    assert 14 == 14
